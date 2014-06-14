35eab0d8c0e6bd48578007476a545094f11a6118
Marcos Placona <marcos.placona@gmail.com>
2014-06-14 23:29:07 +0100

    Fix mocks to go easy on Twillio's API and return messages from fixtures

--- lib/twillio.dart
+++ lib/twillio.dart
@@ -9,14 +9,16 @@ class Twillio {
     String _accountSid;
     String _authToken;
     String _apiVersion;
-    var _httpClient;
+    http.Client _httpClient;
 
     Twillio(String key, String authToken, String version, [http.Client httpClient = null]) {
         this._accountSid = key;
         this._authToken = authToken;
         this._apiVersion = version;
-        print(httpClient.toString());
-        _httpClient = (httpClient == null) ? new http.Client() : httpClient;
+        _httpClient = (httpClient == null) ? new http.Client() : httpClient; 
+        
+        http.Request req = new http.Request('POST', Uri.parse('http://www.google.com'));
+        var s = _httpClient.send(req).then((value) => print(value));
     }
 
     Future sendSMS(String body, String from, String to, [String mediaUrl = ""]) {
@@ -33,18 +35,11 @@ class Twillio {
 
 
     Future _apiRequest(String resource, [Map<String, String> body]) {
-        //return http.post(buildBaseUrl(resource).toString(), body: body).then((response) => response.body);
         var url = buildBaseUrl(resource).toString();
-        //var url = "http://httpbin.org/post";
         var request = new http.Request('POST', Uri.parse(url));
-        //var body = {'Body':'this is a test', 'To':'447590566866', 'From':'441276300056', 'MediaUrl':'http://www.example.com/hearts.png'};
         request.headers[HttpHeaders.USER_AGENT] = 'twilio-dart';
         request.bodyFields = body;
-        //print(new http.Client().toString());
-        //print(_httpClient.toString());
-        //return new http.Client().send(request).then((response) => response.request).catchError((error)=>print(error.toString()));
-        return this._httpClient.send(request).then((response) => response.request).catchError((error)=>print(error.toString()));
-        //return client.send(request).then((response) => response.stream.bytesToString().then((value) => value.toString())).catchError((error) => error.toString());
+        return this._httpClient.send(request).then((response) => response.stream.bytesToString().then((value) => value.toString()));
     }
 
     String buildBaseUrl(String resource) {
--- test/twillio_test.dart
+++ test/twillio_test.dart
@@ -4,18 +4,26 @@ import 'dart:convert';
 import 'dart:async';
 import 'package:http/http.dart' as http;
 import 'package:mock/mock.dart';
-import 'dart:io';
-import 'package:twillio_dart/resources/messages.dart';
-import 'package:twillio_dart/resources/accounts.dart';
 import 'fixtures/status_messages.dart';
+import 'package:twillio_dart/resources/accounts.dart';
+import 'package:twillio_dart/resources/messages.dart';
+import 'package:http/testing.dart';
 
-class HttpClientMock extends Mock implements http.Client {
-  noSuchMethod(i) => super.noSuchMethod(i);
+/*
+void main() {
+  test('bla', () {
+    var mockHttpClient = new HttpClientMock()
+            ..when(callsTo('send')).alwaysReturn(new Future(() => [""]));
+
+    http.Request req = new http.Request('POST', Uri.parse('http://www.google.com'));
+    var s = mockHttpClient.send(req).then((val) => print(val.toString()));
+  });
 }
+*/
 
 void main() {
-  print("Twillio API implementation Tests!");
-
+    print("Twillio API implementation Tests!");
+    /*
   group('Object Setup :: ', () {
     var key = "12345";
     var authToken = "12345";
@@ -30,27 +38,43 @@ void main() {
       expect(twillio is Twillio, isTrue);
     });
   });
+*/
+    group('Methods :: ', () {
+        var _accountSid = "AC9d3e7fbe4b0d27fa1b5c60146fcb3bea",
+                _authToken = "12345",
+                _apiVersion = "2010-04-01",
+                _baseUri = "api.twilio.com";
 
-  group('Methods :: ', () {
-    var _accountSid = "AC9d3e7fbe4b0d27fa1b5c60146fcb3bea",
-        _authToken = "12345",
-        _apiVersion = "2010-04-01",
-        _baseUri = "api.twilio.com";
-    var mockHttpClient = new HttpClientMock()
-        ..when(callsTo('send')).alwaysReturn("this is a test");
-    var twillio = new Twillio(_accountSid, _authToken, _apiVersion, mockHttpClient);
-    Future<http.Request> future;
-    
-    test("New SMS URL is created correctly", () {
-      var accounts = new Accounts().resource;
-      var messages = new Messages(_accountSid);
-      var url = "https://$_accountSid:$_authToken@$_baseUri/$_apiVersion/" + messages.resource;
-      expect(twillio.buildBaseUrl(messages.resource), equals(url));
-    });
+        var mockHttpClient = new MockClient((request) {
+            return new http.Response(message401, 200, headers: {
+                'content-type': 'application/json'
+            });
+        });
 
-    test("Send SMS errors with wrong account", () {
-      future = twillio.sendSMS("+112345678", "+447590566866", "this is a test").then((value) => value.toString());
-      //expect(future.then((value) => JSON.decode(value.body)), completion(equals(JSON.decode(message401))));
+        var twillio = new Twillio(_accountSid, _authToken, _apiVersion, mockHttpClient);
+        Future<http.Request> future;
+
+
+        test("New SMS URL is created correctly", () {
+            var accounts = new Accounts().resource;
+            var messages = new Messages(_accountSid);
+            var url = "https://$_accountSid:$_authToken@$_baseUri/$_apiVersion/" + messages.resource;
+            expect(twillio.buildBaseUrl(messages.resource), equals(url));
+        });
+
+
+        test("Send SMS errors with wrong account", () {
+            var response = JSON.encode({
+                "code": 20003,
+                "detail": "Your AccountSid or AuthToken was incorrect.",
+                "message": "Authenticate",
+                "more_info": "https://www.twilio.com/docs/errors/20003",
+                "status": 401
+            });
+            
+            future = twillio.sendSMS("+112345678", "+447590566866", "this is a test").then((value) => value.toString());
+            expect(future.then((value) => JSON.decode(value)), completion(equals(JSON.decode(response))));
+        });
+
     });
-  });
 }
