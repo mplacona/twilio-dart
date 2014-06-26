be9b09d99b1a9605594e3768b5ca9cf9367e73db
Marcos Placona <marcos.placona@gmail.com>
2014-06-26 21:14:38 +0100

    Add header information to request

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,13 +1,12 @@
 library twilio.utils.utils;
-
 import 'dart:async';
 import 'package:http/http.dart' as http;
-//import 'dart:io';
+import 'http_headers.dart';
 
 Future apiRequest(String resource, http.Client httpClient, Map auth, [String verb = 'GET', Map<String, String> body = null]) {
     var url = buildAuthUrl(resource, auth).toString();
     var request = new http.Request(verb, Uri.parse(url));
-    //request.headers[HttpHeaders.USER_AGENT] = 'twilio-dart';
+    request.headers[HttpHeaders.USER_AGENT] = 'twilio-dart';
     if (body != null) {
         request.bodyFields = body;
     }
